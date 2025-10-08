1f6c23d2ee1af214d4535dfff4656272b986a1cc
PSparling32 <r.sparling32@gmail.com>
2025-10-08 01:57:01 -0700

    Camera Crouch State Integration
    Camera integrated into crouch state

--- Assets/Project/Code/Runtime/Player/State Machine/ConcreteStates/PlayerCrouchState.cs
+++ Assets/Project/Code/Runtime/Player/State Machine/ConcreteStates/PlayerCrouchState.cs
@@ -10,6 +10,9 @@ public class PlayerCrouchState : PlayerState
         var col = motor.GetComponent<SimpleCapsuleResizer>();
         if (col) col.Crouch();
 
+        //camera
+        CameraController.Instance.ApplyActionModifier(CameraController.Instance.crouchCamModifiers);
+
         motor.Mode_Crouch();
         motor.SetHorizontalInput(0f);
         anim.PlayCrouch();
@@ -33,6 +36,9 @@ public class PlayerCrouchState : PlayerState
 
     public override void ExitState()
     {
+        //reset camera
+        CameraController.Instance.RemoveActionModifier(CameraController.Instance.crouchCamModifiers);
+
         motor.SetHorizontalInput(0f);
     }
 }
