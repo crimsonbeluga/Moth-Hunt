303ff05fb542ae86736127c8102de95eaa2c1a4f
Chain <chainbot90@gmail.com>
2025-12-12 18:00:10 -0600

    Fixing some things

--- Assets/Project/Code/Runtime/NoiseMaker.cs
+++ Assets/Project/Code/Runtime/NoiseMaker.cs
@@ -1,32 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NoiseMaker : MonoBehaviour
 {
+    //reference
     public SphereCollider _noiseCollider;
-    public float _volumeDistance;
+
+    [Header("Range")]
+    //hardcoded ranges for noise. Might be replaced with one range and scaled based on state
+    public float _walkSuspicionRange;
+    public float _sprintSuspicionRange;
+    public float _jumpSuspicionRange;
+    public float _crouchSuspicionRange;
+
+    [Header("Terrain")]
+    //set up a multiplier for each type of terrain
+
+    public bool _isOnGrass;
+    public bool _isOnMetal;
+    public bool _isOnDirt;
+    public bool _isOnGravel;
+    public bool _isOnLeaves;
+    public bool _isOnWater;
+
+    [Header("Volume")]
+    public float noiseVolume;
+
+    [Header("Logic")]
     public bool _isNoiseMadeThisFrame;
+    private Ray ray;
+
 
     public void onTick()
     {
+       
+
         //if noise made this frame
         if(_isNoiseMadeThisFrame)
         {
-
-
+            findEnemy(this.transform.position, _noiseCollider.radius);
             //set to false so cant keep at max size
             _isNoiseMadeThisFrame = false;
 
         }
         else
         {
+            
+
             if(_noiseCollider.radius < 1)
             { 
                 _noiseCollider.radius = 1; 
             }
-            _noiseCollider.radius -= .5f;
+            _noiseCollider.radius -= .2f;
         }
+
+        //check if enemy is within range
     }
 
 
@@ -36,4 +66,39 @@ public class NoiseMaker : MonoBehaviour
         _isNoiseMadeThisFrame = true;
         _noiseCollider.radius = vol;
     }
+
+
+
+    public void findEnemy(Vector3 center, float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);//array of all colissions within the sphere
+        foreach (var hitCollider in hitColliders)
+        {// for each collider
+            if (hitCollider.gameObject.layer == 6)// Layer 6 is Enemy, therfore if the game object is on enemy layer
+            {
+                //can safely be removed later
+                Debug.Log("Found An ENEMY!");
+
+                //create a vector for the raycast to follow
+                Vector3 directionToTarget = (hitCollider.transform.position - this.gameObject.transform.position).normalized;
+                //shoot a raycast to the enemy
+                Physics.Raycast(transform.position, directionToTarget, out RaycastHit hitInfo, _noiseCollider.radius);
+                //if raycasts hits the enemy object found in collider
+                if(hitInfo.collider == hitCollider)
+                {
+                    //increase enemy suspicion
+                    hitCollider.gameObject.GetComponent<EnemyBrain>().listen(noiseVolume);
+                }
+                else
+                {
+                    //no line of sight, no noise made. object blocked
+                    //Debug.Log(hitInfo.collider.gameObject.name + " is in the way.");
+                }
+            }
+        }
+    }
+
+
+
+
 }
